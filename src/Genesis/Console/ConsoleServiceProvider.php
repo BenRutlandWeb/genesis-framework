60392f7b948fa5b6001a6d2efc510404a69fc118
Ben Rutland <benrutland@hotmail.com>
2020-09-28 17:42:23 +0100

    added WP_CLI class check

--- src/Genesis/Console/ConsoleServiceProvider.php
+++ src/Genesis/Console/ConsoleServiceProvider.php
@@ -13,6 +13,9 @@ class ConsoleServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
+        if (!class_exists('WP_CLI')) {
+            return;
+        }
         $this->app->singleton('command.royalmail', function () {
             return new \Genesis\Console\Commands\RoyalMail();
         });
