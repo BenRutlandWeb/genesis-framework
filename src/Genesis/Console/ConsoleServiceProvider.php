a8e3f54432309344a33bac20cdd91208ffb6ab92
Ben Rutland <benrutland@hotmail.com>
2020-09-28 18:55:07 +0100

    make in boot method

--- src/Genesis/Console/ConsoleServiceProvider.php
+++ src/Genesis/Console/ConsoleServiceProvider.php
@@ -18,8 +18,23 @@ class ConsoleServiceProvider extends ServiceProvider
         if (!class_exists('WP_CLI')) {
             return;
         }
-        $this->app->instance('command.make.model', new MakeModel($this->app->make('files')));
+        $this->app->singleton('command.make.model', function ($app) {
+            return new MakeModel($app->make('files'));
+        });
 
-        $this->app->instance('command.royalmail', new RoyalMail());
+        $this->app->singleton('command.royalmail', function () {
+            return new RoyalMail();
+        });
+    }
+
+    /**
+     * Boot the service provider.
+     *
+     * @return void
+     */
+    public function boot(): void
+    {
+        $this->app->make('command.make.model');
+        $this->app->make('command.royalmail');
     }
 }
