f6dcd12b6b3e7a1f5c60024b84ae4e706b9c6cf7
Ben Rutland <benrutland@hotmail.com>
2020-09-29 20:22:46 +0100

    added wp-eloquent

---
+++ src/genesis/Database/DatabaseServiceProvider.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Genesis\Database;
+
+use Genesis\Support\ServiceProvider;
+use WPEloquent\Database;
+
+class DatabaseServiceProvider extends ServiceProvider
+{
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register(): void
+    {
+        $this->app->singleton('db', function () {
+
+            return Database::connect();
+        });
+    }
+}
