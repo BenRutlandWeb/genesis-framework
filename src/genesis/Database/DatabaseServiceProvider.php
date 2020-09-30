60a5f6317272943536f0b31550ca8cd9e420a1d7
Ben Rutland <benrutland@hotmail.com>
2020-09-30 17:57:10 +0100

    updated db provider

--- src/genesis/Database/DatabaseServiceProvider.php
+++ src/genesis/Database/DatabaseServiceProvider.php
@@ -4,6 +4,7 @@ namespace Genesis\Database;
 
 use Genesis\Database\Database;
 use Genesis\Support\ServiceProvider;
+use Illuminate\Database\Capsule\Manager as Capsule;
 
 class DatabaseServiceProvider extends ServiceProvider
 {
@@ -14,6 +15,26 @@ class DatabaseServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        $this->app->instance('db', Database::connect());
+        $this->app->singleton('db.capsule', function ($app) {
+            return new Capsule($app);
+        });
+        $this->app->bind('db.connection', function ($app) {
+
+            global $wpdb;
+
+            $instance = $app->make('db.capsule');
+            $instance->addConnection([
+                'driver'    => 'mysql',
+                'prefix'    => $wpdb->prefix,
+                'host'      => DB_HOST,
+                'database'  => DB_NAME,
+                'username'  => DB_USER,
+                'password'  => DB_PASSWORD,
+                'port'      => '3306',
+                'charset'   => 'utf8',
+                'collation' => 'utf8_unicode_ci',
+            ]);
+            $instance->bootEloquent();
+        });
     }
 }
