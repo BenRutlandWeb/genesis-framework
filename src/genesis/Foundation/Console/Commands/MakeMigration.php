1736d0c5df02723b2e6f5c976f616089e82adcbb
Ben Rutland <benrutland@hotmail.com>
2020-10-22 18:00:50 +0100

    added make migration command

---
+++ src/genesis/Foundation/Console/Commands/MakeMigration.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace Genesis\Foundation\Console\Commands;
+
+use Genesis\Console\GenerateCommand;
+use Illuminate\Support\Str;
+
+class MakeMigration extends GenerateCommand
+{
+    /**
+     * The command signature.
+     *
+     * @var string
+     */
+    protected $signature = 'make:migration {name : The name of the migration}
+                                           {--table : The name of the table}
+                                           {--force : Overwrite the event if it exists}';
+
+    /**
+     * The command description.
+     *
+     * @var string
+     */
+    protected $description = 'Make a migration';
+
+    /**
+     * Handle the command call.
+     *
+     * @return void
+     */
+    protected function handle(): void
+    {
+        $name = Str::studly($this->argument('name'));
+
+        $table = $this->option('table');
+
+        $path = $this->getPath(date('Y_m_d_His_') . $name);
+
+        if ($this->files->exists($path) && !$this->option('force')) {
+            $this->error('Migration already exists!');
+        }
+
+        $this->makeDirectory($path);
+
+        $stub = $this->files->get($this->getStub());
+
+        $this->files->put($path, str_replace(['{{ class }}', '{{ table }}'], [$name, $table], $stub));
+
+        $this->success('Migration created');
+    }
+
+    /**
+     * Get the stub path.
+     *
+     * @return string
+     */
+    protected function getStub(): string
+    {
+        if ($this->option('table')) {
+            return __DIR__ . '/stubs/migration.stub';
+        }
+        return __DIR__ . '/stubs/migration.create.stub';
+    }
+
+    /**
+     * Resolve the filepath.
+     *
+     * @param string $name The name of the class.
+     *
+     * @return string
+     */
+    protected function getPath(string $name): string
+    {
+        return app()->basePath("database/migrations/{$name}.php");
+    }
+}
