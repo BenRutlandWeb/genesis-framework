59bfcc99d72dbbdfefe2773aac56dfff5e7d6b19
Ben Rutland <benrutland@hotmail.com>
2020-11-06 21:47:41 +0000

    added isPath method to request

--- src/genesis/Http/Request.php
+++ src/genesis/Http/Request.php
@@ -8,6 +8,18 @@ use JsonSerializable;
 class Request extends IlluminateRequest implements JsonSerializable
 {
     /**
+     * Compare the request path with the given path for a match
+     *
+     * @param string $path
+     *
+     * @return boolean
+     */
+    public function isPath(string $path): bool
+    {
+        return trim($path, '/') === trim($this->path(), '/');
+    }
+
+    /**
      * Convert the request into a json string
      *
      * @return string
